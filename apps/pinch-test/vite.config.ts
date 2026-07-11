cb3b1a12798f4d072394bf2b03b43d7471d14c92
Winston Fassett <dev@winstonfassett.com>
2026-07-11 09:36:21 -0500

    refactor: remove pinch-test app

--- apps/pinch-test/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: { host: true, port: 5199 },
-  build: {
-    minify: 'terser',
-    terser: { keep_classnames: true },
-  },
-});
